79515ca74bc6b493534772f8ffae9ff058921b86
gulfemakkaya <glfmakya@gmail.com>
2022-12-12 12:19:40 +0100

    add character-card function

--- components/card/card.js
+++ components/card/card.js
@@ -1,2 +1,67 @@
-export function createCharacterCard() {}
-console.log();
+export function createCharacterCard() {
+  console.log("hi");
+  const card = document.createElement("li");
+  card.classList.add("card");
+
+  const cardImageContainer = document.createElement("div");
+  cardImageContainer.classList.add("card__image-container");
+
+  const cardImage = document.createElement("img");
+  cardImage.classList.add("card__image");
+  cardImage.setAttribute(
+    "src",
+    "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  );
+  cardImage.setAttribute("alt", "Rick Sanches");
+
+  const cardImageGradient = document.createElement("div");
+  cardImageGradient.classList.add("card__image-gradient");
+
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card__content");
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card__title");
+  cardTitle.textContent = "Rick Sanchez";
+
+  const cardInfo = document.createElement("dl");
+  cardInfo.classList.add("card__info");
+
+  const cardInfoTitle1 = document.createElement("dt");
+  cardInfoTitle1.classList.add("card__info-title");
+  cardInfoTitle1.textContent = "Status";
+
+  const cardInfoDescription1 = document.createElement("dd");
+  cardInfoDescription1.classList.add("card__info-description");
+  cardInfoDescription1.textContent = "Alive";
+
+  const cardInfoTitle2 = document.createElement("dt");
+  cardInfoTitle2.classList.add("card__info-title");
+  cardInfoTitle2.textContent = "Type";
+
+  const cardInfoDescription2 = document.createElement("dd");
+  cardInfoDescription2.classList.add("card__info-description");
+  cardInfoDescription2.textContent = "Blabla";
+
+  const cardInfoTitle3 = document.createElement("dt");
+  cardInfoTitle3.classList.add("card__info-title");
+  cardInfoTitle3.textContent = "Occurences";
+
+  const cardInfoDescription3 = document.createElement("dd");
+  cardInfoDescription3.classList.add("card__info-description");
+  cardInfoDescription3.textContent = "51";
+
+  card.append(cardImageContainer);
+  cardImageContainer.append(cardImageGradient);
+  cardImageContainer.append(cardImage);
+  card.append(cardContent);
+  cardContent.append(cardTitle);
+  cardContent.append(cardInfo);
+  cardInfo.append(cardInfoTitle1);
+  cardInfo.append(cardInfoDescription1);
+  cardInfo.append(cardInfoTitle2);
+  cardInfo.append(cardInfoDescription2);
+  cardInfo.append(cardInfoTitle3);
+  cardInfo.append(cardInfoDescription3);
+  return card;
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { createCharacterCard } from "./components/card/card.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +13,7 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const cardItem = createCharacterCard();
+console.log(cardItem);
+cardContainer.append(cardItem);
